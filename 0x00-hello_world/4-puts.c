8cc2f959c6e9a01e9d2e67d139cf47f74ce8b9f4
wafaa0hassan <wafaa.abdelwahid12@gmail.com>
2023-02-16 04:36:01 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * main - prints "Programming is like building a multilingual
+ *  puzzle," followed by a newline.
  * Return: Always 0.
  */
 
@@ -9,5 +10,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
