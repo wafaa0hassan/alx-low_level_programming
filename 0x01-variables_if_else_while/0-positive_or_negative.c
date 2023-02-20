6cab0d2648f520e09d2145f0fb294d252c231fa5
wafaa0hassan <wafaa.abdelwahid12@gmail.com>
2023-02-20 10:58:15 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand()-RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -32,6 +32,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return 0;
+	return (0);
 
 }
