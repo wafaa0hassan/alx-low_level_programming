c44185dfd96c843c1fc8c713298f33cd3900bc6e
wafaa0hassan <wafaa.abdelwahid12@gmail.com>
2023-02-20 11:29:12 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase,
+ * followed by a new line.
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c<= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+
+}
